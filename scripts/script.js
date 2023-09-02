7ce610bb3688ee07f64d6837a02936c494e92a1a
hatef <hatefathi@gmail.com>
2023-09-02 10:10:57 +0330

    adding to top button

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 const navbar = document.querySelector(".navbar");
+const toTopButton = document.querySelector("#to-top");
+
+const scrollToTop = function () {
+  // for safari
+  document.body.scrollTop = 0;
+  // for other browsers
+  document.documentElement.scrollTop = 0;
+};
 
 const userScroll = function () {
   window.addEventListener("scroll", () => {
@@ -6,11 +14,16 @@ const userScroll = function () {
       navbar.classList.add("navbar--scrolled");
       navbar.classList.add("navbar-sticky");
       navbar.classList.add("sticky-top");
+      toTopButton.classList.add("to-top-btn--show");
     } else {
       navbar.classList.remove("navbar--scrolled");
       navbar.classList.remove("navbar-sticky");
       navbar.classList.remove("sticky-top");
+      toTopButton.classList.remove("to-top-btn--show");
     }
   });
 };
+
+// Event listeners
 document.addEventListener("DOMContentLoaded", userScroll);
+toTopButton.addEventListener("click", scrollToTop);
